bfd137d09a6505b7f5809fc56d302c41b94f432b
David Morilla-Cabello <davidmorillacabello@gmail.com>
2025-10-08 16:09:43 +0200

    Fix

--- drones_as2_unizar/src/vision_pose_republisher.cpp
+++ drones_as2_unizar/src/vision_pose_republisher.cpp
@@ -2,7 +2,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include "geometry_msgs/msg/twist_stamped.hpp"
-#include "rclcpp/qos.hpp"
 
 using std::placeholders::_1;
 
@@ -12,38 +11,37 @@ public:
     VisionPoseRepublisher()
         : Node("vision_pose_republisher")
     {
-        // Get node namespace (e.g., "/drone0")
-        std::string ns = this->get_namespace();
-        if (ns.empty()) ns = "/";
-
-        // Declare parameters with defaults (without namespace prefix)
+        // Declare default parameters (relative topic names)
         this->declare_parameter<std::string>("pose_in_topic", "self_localization/pose");
         this->declare_parameter<std::string>("twist_in_topic", "self_localization/twist");
         this->declare_parameter<std::string>("pose_out_topic", "mavros/vision_pose/pose");
         this->declare_parameter<std::string>("twist_out_topic", "mavros/vision_speed/speed_twist");
 
-        // Get parameter values
-        std::string pose_in  = ns + "/" + this->get_parameter("pose_in_topic").as_string();
-        std::string twist_in = ns + "/" + this->get_parameter("twist_in_topic").as_string();
-        std::string pose_out = ns + "/" + this->get_parameter("pose_out_topic").as_string();
-        std::string twist_out = ns + "/" + this->get_parameter("twist_out_topic").as_string();
+        std::string pose_in = this->get_parameter("pose_in_topic").as_string();
+        std::string twist_in = this->get_parameter("twist_in_topic").as_string();
+        std::string pose_out = this->get_parameter("pose_out_topic").as_string();
+        std::string twist_out = this->get_parameter("twist_out_topic").as_string();
 
-        // QoS: Reliable
-        rclcpp::QoS qos(rclcpp::KeepLast(10));
-        qos.reliable();
+        // Input QoS (matches upstream publisher)
+        rclcpp::QoS in_qos(10);
+        in_qos.best_effort();  // or whatever the localization publisher uses
+
+        // Output QoS (matches downstream subscriber, MAVROS)
+        rclcpp::QoS out_qos(10);
+        out_qos.reliable();
 
         // Subscribers
         pose_sub_ = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-            pose_in, qos, std::bind(&VisionPoseRepublisher::pose_callback, this, _1));
+            pose_in, in_qos, std::bind(&VisionPoseRepublisher::pose_callback, this, _1));
         twist_sub_ = this->create_subscription<geometry_msgs::msg::TwistStamped>(
-            twist_in, qos, std::bind(&VisionPoseRepublisher::twist_callback, this, _1));
+            twist_in, in_qos, std::bind(&VisionPoseRepublisher::twist_callback, this, _1));
 
         // Publishers
-        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(pose_out, qos);
-        twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(twist_out, qos);
+        pose_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(pose_out, out_qos);
+        twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(twist_out, out_qos);
 
-        RCLCPP_INFO(this->get_logger(), "Subscribed: %s / %s", pose_in.c_str(), twist_in.c_str());
-        RCLCPP_INFO(this->get_logger(), "Publishing: %s / %s", pose_out.c_str(), twist_out.c_str());
+        RCLCPP_INFO(this->get_logger(), "Subscribed (input QoS): %s / %s", pose_in.c_str(), twist_in.c_str());
+        RCLCPP_INFO(this->get_logger(), "Publishing (output QoS): %s / %s", pose_out.c_str(), twist_out.c_str());
     }
 
 private:
@@ -63,7 +61,7 @@ private:
     rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
 };
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     auto node = std::make_shared<VisionPoseRepublisher>();
